2100d2bc75dfc359ff8a98055e485fafd789877d
nfoert <nofoert@gmail.com>
2023-04-15 12:06:52 -0400

    Add debug to update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
-sleep 3
-taskkill /F /IM "Jade Launcher.exe"
-del /F "Jade Launcher.exe"
-ren "Jade Launcher.exe.download" "Jade Launcher.exe"
-"Jade Launcher.exe"
+sleep 3 > sleep.txt
+taskkill /F /IM "Jade Launcher.exe" > taskkill.txt
+del /F "Jade Launcher.exe" > delete.txt
+ren "Jade Launcher.exe.download" "Jade Launcher.exe" > rename.txt
+"Jade Launcher.exe" > run.txt
